d891bdf11ef2d752751e2190fe0bf496eef28495
Sabuj Mondal <hallosabuj@gmail.com>
2022-06-23 13:26:44 +0530

    ..

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -16,6 +17,28 @@ const (
 
 type server struct {
 	proto.UnimplementedGreeterServer
+}
+
+// SayHello()
+func (s *server) SayHello(ctx context.Context, in *proto.HelloRequest) (*proto.HelloReply, error) {
+	// pass
+	log.Println("Received :", in.GetName())
+	return &proto.HelloReply{Message: "Hello Sabuj"}, nil
+}
+
+// EchoGET()
+func (s *server) EchoGET(ctx context.Context, in *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{Message: "Hello User"}, nil
+}
+
+// EchoPOST()
+func (s *server) EchoPOST(ctx context.Context, in *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+// EchoName()
+func (s *server) EchoName(ctx context.Context, in *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
 func main() {
